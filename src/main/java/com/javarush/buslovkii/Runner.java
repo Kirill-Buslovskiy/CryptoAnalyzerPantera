e641c314835d2d57929114e1b40fed7d7095d7d2
Kirill Buslovskii <k.buslovsky@yandex.ru>
2025-08-25 22:36:44 +0300

    Start project
    add alphabet and scanner

---
+++ src/main/java/com/javarush/buslovkii/Runner.java
@@ -0,0 +1,16 @@
+package com.javarush.buslovkii;
+
+import java.util.Scanner;
+
+public class Runner {
+    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'и', 'о', 'п', 'р',
+            'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '"', '\'', ':', '-', '!', '?', ' '};
+
+    public static void main(String[] args) {
+        Scanner inputMode = new Scanner(System.in);
+        
+
+
+
+    }
+}
