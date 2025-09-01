dfc7665cc6488de05df854fe8afd399d6e039db5
Kirill Buslovskii <k.buslovsky@yandex.ru>
2025-09-01 22:13:45 +0300

    ADD classes File and KeyExceptions

---
+++ src/main/java/com/javarush/buslovskii/exceptions/FileExceptions.java
@@ -0,0 +1,7 @@
+package com.javarush.buslovskii.exceptions;
+
+public class FileExceptions extends RuntimeException {
+    public FileExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
